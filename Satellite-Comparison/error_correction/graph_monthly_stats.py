d08d2258634d22c6c6184f218222e058b4964e85
mlysack <lysackm5@gmail.com>
2023-08-15 12:41:47 -0500

    graphs for monthly stats ascetic updates

--- Satellite-Comparison/error_correction/graph_monthly_stats.py
+++ Satellite-Comparison/error_correction/graph_monthly_stats.py
@@ -1,54 +1,54 @@
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import pandas as pd
 import json
 
 from matplotlib import cycler
 
 
 def graph_year_both_models(stats):
-    colours = ["tab:orange", "peru", "tomato", "darkred", "tab:blue", "steelblue", "turquoise", "navy"]
     plt.style.use("ggplot")
     plt.rc("axes",
-           prop_cycle=(cycler('color', colours) + cycler('linestyle', ['-', '--', ':', '-.', '-', '--', ':', '-.'])))
+           prop_cycle=(cycler('linestyle', ['-', '--', ':', '-.'])))
 
-    for attr in stats:
+    file_names = ["RMSE_AvgAir_T", "RMSE_AvgWS", "RMSE_Pluvio_Rain", "RMSE_Press_hPa", "RMSE_RH", "RMSE_SolarRad"]
+
+    for attr, file_name in zip(stats, file_names):
         for model in stats[attr]:
             for data_type in stats[attr][model]:
                 y_data = stats[attr][model][data_type]
                 x_data = range(12)
 
-                plt.plot(x_data, y_data, label=model + " " + data_type)
+                colour = "orange" if model == "merra" else "blue"
+
+                plt.plot(x_data, y_data, label=model + " " + data_type, c=colour)
 
         months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         plt.xticks(range(12), months)
 
         plt.title(attr)
-        plt.savefig("stats/" + attr + model + '.png')
+        plt.savefig("stats/" + file_name + '.png')
         plt.clf()
 
 
+# graph each model separately. That is that each graph is one model
+# with 4 types of data on 1 attribute.
 def graph_year(stats):
-    colours_merra = ["tab:orange", "peru", "tomato", "darkred"]
-    colours_era5 = ["tab:blue", "steelblue", "turquoise", "navy"]
     plt.style.use("ggplot")
 
     for model in stats:
         for attr in stats[model]:
             if model == "MERRA":
-                colours = colours_merra
+                colour = "orange"
             else:
-                colours = colours_era5
+                colour = "blue"
 
             plt.rc("axes",
-                   prop_cycle=(cycler('color', colours) + cycler('linestyle',
-                                                                      ['-', '--', ':', '-.', ])))
+                   prop_cycle=(cycler('linestyle', ['-', '--', ':', '-.', ])))
 
             for data_type in stats[model][attr]:
                 y_data = stats[model][attr][data_type]
                 x_data = range(12)
 
-                plt.plot(x_data, y_data, label=data_type)
+                plt.plot(x_data, y_data, label=data_type, c=colour)
 
             months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
             plt.xticks(range(12), months)
@@ -56,7 +56,7 @@ def graph_year(stats):
             plt.title(attr + " " + model)
             plt.legend()
             # plt.show()
-            plt.savefig("stats/" + attr + model + '.png')
+            # plt.savefig("stats/" + attr + model + '.png')
             plt.clf()
 
 
