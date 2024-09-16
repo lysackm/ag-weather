185b839383b1c4e5f7f38b4c119390387cd8a779
mlysack <lysackm5@gmail.com>
2024-09-16 09:52:07 -0500

    Made logic cleaner

--- prod_server/email_notification/server_email_notification.py
+++ prod_server/email_notification/server_email_notification.py
@@ -7,7 +7,6 @@ import hashlib
 
 email_sender = "mbagWeather@outlook.com"
 email_recipient = ["Mark.Lysack@gov.mb.ca", "lysackm@myumanitoba.ca", "a_sass3@hotmail.com", "alison.sass@gov.mb.ca"]
-# email_recipient = ["Mark.Lysack@gov.mb.ca"]
 smtp_server = "smtp-mail.outlook.com"
 smtp_port = 587
 
@@ -24,23 +23,12 @@ def has_sent_mail(metadata):
 
 
 def get_curr_image_hash():
-    r = requests.get("https://mbagweather.ca/partners/rtmc/Carman12.png")
+    r = requests.get("https://mbagweather.ca/partners/rtmc/Carman212.png")
     new_image = r.content
-    # save new file
-    with open("new_image.jpg", "wb") as file:
-        file.write(new_image)
 
-    with open("new_image.jpg", "r") as file:
-        image_hash = hashlib.sha256(new_image).hexdigest()
+    image_hash = hashlib.sha256(new_image).hexdigest()
 
     return image_hash
-
-
-def same_image_hashes(metadata):
-    old_hash = metadata["image_hash"]
-    new_hash = get_curr_image_hash()
-
-    return old_hash == new_hash
 
 
 def save_metadata(sent_mail, last_updated, image_hash):
@@ -80,24 +68,16 @@ def main():
     image_hash = get_curr_image_hash()
 
     date_delta = date - live_page_date
-    two_hours = datetime.timedelta(hours=2)
+    time_threshold = datetime.timedelta(minutes=19)
 
-    if date_delta >= two_hours:
-        # check if the hash is different
-        if same_image_hashes(metadata):
-            # check if an email has already been sent
-            if not has_sent_mail(metadata):
-                print("sending email")
-                send_email(str(live_page_date))
-                # sent mail, same, same
-                save_metadata("true", metadata["last_updated"], metadata["image_hash"])
-        else:
-            # has not sent mail, update date, hash is different
-            save_metadata("false", date_string, image_hash)
+    if image_hash != metadata["image_hash"]:
+        # dont send mail, update data
+        save_metadata("false", date_string, image_hash)
     else:
-        if not same_image_hashes(metadata):
-            # not sent mail, update date, update hash
-            save_metadata("false", date_string, image_hash)
+        if date_delta >= time_threshold and not metadata["sent_mail"]:
+            print("sending email")
+            send_email(str(live_page_date))
+            save_metadata("true", metadata["last_updated"], metadata["image_hash"])
 
 
 if __name__ == "__main__":
